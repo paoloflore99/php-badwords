df580907f2fd36068841607d7f28a0bec50d05e9
paoloflore99 <paoloflore937@gmail.com>
2023-09-07 09:21:20 +0200

    finito

--- ricevitore.php
+++ ricevitore.php
@@ -5,9 +5,12 @@
 
 
 echo 'ciao'. " " .$_POST['name'] . ' ' . $_POST['cognome'];
-var_dump($_POST);
 
+ //cosi facendo mi stampa solo *** aggiuntivi al name
+var_dump($_POST);             //sarebbel il consol.log() in php
 
+echo $_POST['name'] = '***'; 
+//fare una condizzione ?
 ?>
 
 
